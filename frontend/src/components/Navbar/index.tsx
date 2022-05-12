b1adfa80fc630837f7a6bff3a5291096c3d13b4c
felipeperaltacheung <f33___@hotmail.com>
2022-05-12 08:25:10 -0300

    navbar e seu stylescss

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,23 @@
 import {ReactComponent as GitHubIcon} from 'assests/img/gitIcon.svg';
-
+import './styles.css';
 
 function Navbar(){
 
-    return(
-        <header>
-        <nav>
-          <div>
+  return (
+    <header>
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
           <h1>DSMovie</h1>
           <a href="www.pudim.com.br" >
-          <div>
-            <GitHubIcon />
-            <p>/devsuperior</p>
-          </div>
+            <div className='dsmovie-contact-container'>
+              <GitHubIcon />
+              <p className='dsmovie-contact-link'>/devsuperior</p>
+            </div>
           </a>
-          </div>
-        </nav>
-      </header>
-    );
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
